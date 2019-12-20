3c469c0d78ff89408f1a9b14a1dcdb332fde9c95
zhuying <zhuying@cyberzone>
2019-12-20 17:44:36 +0800

    first

---
+++ src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+// vant组件
+import { Button } from 'vant';
+import { Tab, Tabs } from 'vant';
+import { Tabbar, TabbarItem  } from 'vant';
+import { Swipe, SwipeItem,ImagePreview,DropdownMenu, DropdownItem } from 'vant';
+import { Row, Col } from 'vant';
+import { Panel } from 'vant';
+import { Icon,List,Cell, CellGroup,Picker,DatetimePicker,NumberKeyboard,PasswordInput,Field,NoticeBar,Dialog,Circle ,Progress, Tag,RadioGroup,Radio, PullRefresh} from 'vant';
+import { NavBar } from 'vant';
+import { Toast } from 'vant';
+import { Popup,Uploader } from 'vant';
+Vue.use(Button).use(Tab).use(Tabs).use(Tabbar).use(TabbarItem).use(Swipe).use(SwipeItem).use(Row).use(Col).use(Panel).use(Icon).use(NavBar).use(List).use(Cell).use(CellGroup).use(Popup).use(Picker).use(DatetimePicker).use(NumberKeyboard).use(PasswordInput).use(Field).use(NoticeBar).use(Dialog).use(Circle).use(Progress).use(Tag).use(RadioGroup).use(Radio).use(Uploader).use(ImagePreview).use(DropdownMenu).use(DropdownItem).use(PullRefresh);
+
+Vue.use(VueAxios,axios);
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
